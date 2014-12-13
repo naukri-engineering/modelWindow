e4585add22a1abdca9bf5981d7ad6591072db946
ankit.anand <ankit.anand@naukri.com>
2014-12-13 21:34:30 +0530

    index.html

--- j/demo.js
+++ j/demo.js
@@ -18,3 +18,22 @@ $('#trigger4').lightBox({
 	"resetForm":true,
 	"fixed": true
 });
+
+$('#trigger5').lightBox({
+	"ltBox":$('#container5'),
+	"dimens":{"width":"50%"},
+	"resetForm":true,
+	"fixed": true,
+	"open":{"anim":{"className":"flipOpen"}},
+	"close":{"anim":{"className":'flipClose',"duration":300}}
+});  
+
+$('#trigger6').lightBox({
+	"ltBox":$('#container6'),
+	"dimens":{"width":"50%"},
+	"resetForm":true,
+	"fixed": true,
+	"open":{"anim":{"className":"flipOpen"}},
+	"close":{"nodes":$("#container6 .close"),//{"target":$("#container6"),"selector":".close"} is also valid
+	"anim":{"className":'flipClose',"duration":300}}	
+});  
